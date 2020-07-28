0a22c4bc266d6a560a4465f8dc9efedbf197d418
Elena Hohlova <67377522+Elena085@users.noreply.github.com>
2020-07-28 03:30:12 +0300

    7/3

--- pr7.cpp
+++ pr7.cpp
@@ -39,3 +39,44 @@ void output_matrix(int matrix[][5], int *X, int Y, int m, int n) {
 	fin << "\nY = " << Y;
 	fin.close();
 }
+int calculate_Y_rec(int *ptr, int min, int n,int k) {
+	if (n > 0) {
+		if (*ptr==min) {
+			
+			k++;
+			return calculate_Y_rec(ptr + 1, min, n - 1,k);
+		}
+		else if (*ptr<min ) {
+			min = *ptr;
+			k = 1;
+			return calculate_Y_rec(ptr + 1, min, n - 1,k);
+		}
+		return calculate_Y_rec(ptr + 1, min, n - 1,k);
+	}
+	return k;
+}
+int calculate_Y(int *ptr, int n) {
+	int min = *ptr;
+	int k = 1;
+	return calculate_Y_rec(ptr+1, min, n-1,k);
+}
+int* solution(void(*input_matrix)(int(*)[5], int, int)) {
+#define N 5
+#define M 5
+	int A[M][N];
+	int X[M] = { 0 };
+	int Y;
+	input_matrix(A, M, N);
+	Y = calculate_Y(*A, N);
+	int max = A[0][0];
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N&&A[i][j]>=0; j++) {
+			X[i] += A[i][j];
+		}
+	}
+	
+	output_matrix(A, X, Y, M, N);
+	return X;
+#undef N 
+#undef M 
+}
